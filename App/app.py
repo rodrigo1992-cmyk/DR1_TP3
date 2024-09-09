1ab51f50a63ac58bdfdc90681d4ce42c87e6ed8b
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-09-09 09:08:11 -0300

    ok

---
+++ App/app.py
@@ -0,0 +1,45 @@
+
+import pandas as pd
+import streamlit as st
+import regex as re
+
+df = pd.DataFrame()
+files = st.file_uploader('Upload', type='csv', accept_multiple_files=True)
+
+list_continentes = ['África','América Central','América do Norte', 'América do Sul', 'Ásia', 'Europa', 'Oriente Médio', 'Oceania']
+
+
+if files is not None:
+    for file in files:
+        data = pd.read_csv(file)
+        st.write(data)
+        #Primeiro obtive a identificação do ano, que está dentro de uma frase na terceira linha
+        line = data.iloc[1][0]
+        #Utilizo Regex para extrair o ano
+        Ano = re.findall(r'\b(20[0-9][0-9])\b', line)[0]   
+        #Depois trato o restante do arquivo para ficar no formato desejado    
+        data = data.iloc[4:]
+        data.iloc[0][0] = 'Pais'
+        data.columns = data.iloc[0]
+        data = data.drop(data.index[0])
+        data = data.dropna()
+        #Retiro a coluna e a linha de total
+        data = data[data['Pais'] != 'Total']
+        data = data.drop(columns='Total')
+        data['Ano'] = Ano
+
+        #Utilizei IA para criar a função que identifica o continente (Ps: Estou com muita dificuldade para criar funções lambda)
+        data['Continente'] = data['Pais'].apply(
+            lambda line: next((pais for pais in list_continentes if pais in line), None)
+        )
+        data['Continente'] = data['Continente'].fillna(method='ffill')
+
+        #Por fim, retiro as linhas que contém o total de cada continente, deixando apenas os países
+        data = data[~data['Pais'].isin(list_continentes)]
+
+        #Concateno em um dataframe, pois assim permito que o usuário faça a importação de diversos CSVs, um para cada ano
+        df = pd.concat([df, data])
+
+df_melt = df.melt(id_vars=['Pais', 'Ano', 'Continente'], var_name='Mes', value_name='Qtd')
+
+df_melt.to_csv(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR1_TP3\Data\arquivo_carregado.csv', index=False)
