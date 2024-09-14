dda28e2ba70cd45e3c2f7e03234dc1d437113268
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-09-14 12:41:16 -0300

    ok

--- App/app.py
+++ App/app.py
@@ -3,103 +3,131 @@ import pandas as pd
 import streamlit as st
 import regex as re
 import time
+from st_aggrid import AgGrid, GridOptionsBuilder
 
-#Adicionando o Color Picker
-col1, col2 = st.columns(2)
+def barra_lateral():
+    st.sidebar.header('Filtros')
+    #Adicionando o Color Picker
+    cor_fundo = st.sidebar.color_picker("Cor de fundo", "#FFFFFF")
+    cor_fonte = st.sidebar.color_picker("Cor da fonte", "#000000")  
 
-with col1:
-    cor_fundo = st.color_picker("Escolha a cor de fundo", "#FFFFFF")  
+    #Alterando o estilo da classe stApp
+    st.markdown(f"<style> .stApp {{background-color: {cor_fundo}; color:{cor_fonte};}}   </style>", unsafe_allow_html=True)
 
-with col2:
-    cor_fonte = st.color_picker("Escolha a cor da fonte", "#000000")  
+def upload():
+    files = st.file_uploader('Upload', type='csv', accept_multiple_files=True)
+    return files
 
-#Alterando o estilo da classe stApp
-st.markdown(f"<style> .stApp {{background-color: {cor_fundo}; color:{cor_fonte};}}   </style>", unsafe_allow_html=True)
+@st.cache_data
+def tratamento(files):
+    
+    df = pd.DataFrame()
+    list_continentes = ['África','América Central','América do Norte', 'América do Sul', 'Ásia', 'Europa', 'Oriente Médio', 'Oceania']
+    
+    if files != []:
+        #Adicionando o Spinner
+        with st.spinner('Upload em andamento...'):
+            time.sleep(2)
+            st.success("Upload concluído!")
 
+        for file in files:
+            data = pd.read_csv(file)
+            #Primeiro obtive a identificação do ano, que está dentro de uma frase na terceira linha
+            line = data.iloc[1][0]
+            #Utilizo Regex para extrair o ano
+            Ano = re.findall(r'\b(20[0-9][0-9])\b', line)[0]   
+            #Depois trato o restante do arquivo para ficar no formato desejado    
+            data = data.iloc[4:]
+            data.iloc[0][0] = 'Pais'
+            data.columns = data.iloc[0]
+            data = data.drop(data.index[0])
+            data = data.dropna()
+            #Retiro a coluna e a linha de total
+            data = data[data['Pais'] != 'Total']
+            data = data.drop(columns='Total')
+            data['Ano'] = Ano
 
-df = pd.DataFrame()
-files = st.file_uploader('Upload', type='csv', accept_multiple_files=True)
+            #Utilizei IA para criar a função que identifica o continente (Ps: Estou com muita dificuldade para criar funções lambda)
+            data['Continente'] = data['Pais'].apply(
+                lambda line: next((pais for pais in list_continentes if pais in line), None)
+            )
+            data['Continente'] = data['Continente'].fillna(method='ffill')
 
-list_continentes = ['África','América Central','América do Norte', 'América do Sul', 'Ásia', 'Europa', 'Oriente Médio', 'Oceania']
+            #Por fim, retiro as linhas que contém o total de cada continente, deixando apenas os países
+            data = data[~data['Pais'].isin(list_continentes)]
 
+            data = data.melt(id_vars=['Continente', 'Pais', 'Ano'], var_name='Mes', value_name='Qtd')
 
-if files != []:
-    #Adicionando o Spinner
-    with st.spinner('Upload em andamento...'):
-        time.sleep(2)
-        st.success("Upload concluído!")
+            #Concateno em um dataframe, pois assim permito que o usuário faça a importação de diversos CSVs, um para cada ano
+            df = pd.concat([df, data])
 
-    for file in files:
-        data = pd.read_csv(file)
-        #Primeiro obtive a identificação do ano, que está dentro de uma frase na terceira linha
-        line = data.iloc[1][0]
-        #Utilizo Regex para extrair o ano
-        Ano = re.findall(r'\b(20[0-9][0-9])\b', line)[0]   
-        #Depois trato o restante do arquivo para ficar no formato desejado    
-        data = data.iloc[4:]
-        data.iloc[0][0] = 'Pais'
-        data.columns = data.iloc[0]
-        data = data.drop(data.index[0])
-        data = data.dropna()
-        #Retiro a coluna e a linha de total
-        data = data[data['Pais'] != 'Total']
-        data = data.drop(columns='Total')
-        data['Ano'] = Ano
+            #Converto para CSV
+            csv = df.to_csv().encode("utf-8")
 
-        #Utilizei IA para criar a função que identifica o continente (Ps: Estou com muita dificuldade para criar funções lambda)
-        data['Continente'] = data['Pais'].apply(
-            lambda line: next((pais for pais in list_continentes if pais in line), None)
-        )
-        data['Continente'] = data['Continente'].fillna(method='ffill')
+        return df, csv
 
-        #Por fim, retiro as linhas que contém o total de cada continente, deixando apenas os países
-        data = data[~data['Pais'].isin(list_continentes)]
+def filtra_df(df):
+    if df is not None:
+        
+        #Inicializo o session_state
+        if 'filter_cont' not in st.session_state:
+            st.session_state.filter_cont = 'Todos'
+        if 'list_colunas' not in st.session_state:
+            st.session_state.list_colunas = df.columns.tolist()
 
-        data = data.melt(id_vars=['Continente', 'Pais', 'Ano'], var_name='Mes', value_name='Qtd')
+        #Crio a lista de continentes
+        list_cont = df['Continente'].drop_duplicates().tolist()
+        list_cont.append('Todos')
 
-        #Concateno em um dataframe, pois assim permito que o usuário faça a importação de diversos CSVs, um para cada ano
-        df = pd.concat([df, data])
+        #Crio o filtro de continentes
+        st.session_state.filter_cont = st.sidebar.selectbox('Filtrar Continentes', list_cont, index=list_cont.index(st.session_state.filter_cont))
 
-    #Crio a lista de continentes
-    list_cont = df['Continente'].drop_duplicates().tolist()
-    list_cont.append('Todos')
+        #Aplico a condição para só filtrar se houver seleção
+        if st.session_state.filter_cont != 'Todos':
+            df = df.loc[df['Continente'] == st.session_state.filter_cont]
 
-    #Pego o id da opção "Todos"
-    default_cont = list_cont.index('Todos')
+        #Crio o filtro de colunas
+        st.session_state.list_colunas = st.sidebar.multiselect('Selecione as colunas', df.columns, st.session_state.list_colunas)
 
-    #Crio o filtro de continentes
-    filter_cont = st.selectbox('Filtrar Continentes', list_cont, index=default_cont)
+        #Aplico o filtro
+        df_filt = df[st.session_state.list_colunas]
 
-    #Aplico a condição para só filtrar se houver seleção
-    if filter_cont != 'Todos':
-        df = df.loc[df['Continente'] == filter_cont]
+        #Adicionando a barra de progresso
+        my_bar = st.progress(0, text="Carregando a Visualização...")
+        for percent_complete in range(100):
+            time.sleep(0.001)
+            my_bar.progress(percent_complete + 1, text="Carregando a Visualização...")
+        time.sleep(0.5)
+        my_bar.empty()
+    
+        return df_filt
 
-    #Crio o filtro de colunas
-    list_colunas = st.multiselect('Selecione as colunas', df.columns, df.columns)
-    #Aplico o filtro
-    df = df[list_colunas]
+def exibe_tabelas(df_filt):
+    st.write('## Tabela Comum')
+    st.dataframe(df)
 
-    #Adicionando a barra de progresso
-    my_bar = st.progress(0, text="Carregando a Visualização...")
-    for percent_complete in range(100):
-        time.sleep(0.005)
-        my_bar.progress(percent_complete + 1, text="Carregando a Visualização...")
-    time.sleep(0.5)
-    my_bar.empty()
+    st.write('## Tabela Interativa')
+    if df_filt is not None:
+        gb = GridOptionsBuilder.from_dataframe(df_filt)
+        gb.configure_default_column(editable=False, groupable=True)
+        gb.configure_selection('multiple', use_checkbox=True)
+        gridOptions = gb.build()
 
-    #Mostro a tabela filtrada
-    st.write(df) 
+        AgGrid(df_filt, gridOptions=gridOptions, enable_enterprise_modules=True)
 
-    #Converto para CSV
-    csv = df.to_csv().encode("utf-8")
+################### INICIANDO O APP ######################
 
-    #Crio botão para baixar o arquivo csv
-    st.download_button(
-        label="Baixar Dados",
-        data=csv,
-        file_name="base.csv",
-        mime="text/csv",
-    )
+barra_lateral()
+files = upload()
 
+start = time.time()
 
+if files:
+    df, csv = tratamento(files)
+    df_filt = filtra_df(df)
+    exibe_tabelas(df_filt)
+    st.download_button(label="Baixar Dados", data=csv, file_name="base.csv", mime="text/csv")
+
+end = time.time()
+st.write(f'Tempo de execução: {end - start:.2f} segundos')
     
