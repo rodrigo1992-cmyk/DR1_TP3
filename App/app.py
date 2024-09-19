ab30ddd0b89c62cfa5948e2a848c42c32fed66a5
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-09-18 22:01:44 -0300

    ok

--- App/app.py
+++ App/app.py
@@ -38,12 +38,14 @@ def tratamento(files):
             line = data.iloc[1][0]
             #Utilizo Regex para extrair o ano
             Ano = re.findall(r'\b(20[0-9][0-9])\b', line)[0]   
+
             #Depois trato o restante do arquivo para ficar no formato desejado    
             data = data.iloc[4:]
             data.iloc[0][0] = 'Pais'
             data.columns = data.iloc[0]
             data = data.drop(data.index[0])
             data = data.dropna()
+
             #Retiro a coluna e a linha de total
             data = data[data['Pais'] != 'Total']
             data = data.drop(columns='Total')
@@ -60,6 +62,10 @@ def tratamento(files):
 
             data = data.melt(id_vars=['Continente', 'Pais', 'Ano'], var_name='Mes', value_name='Qtd')
 
+            #Convertendo o campo Qtd para número
+            data['Qtd'] = data['Qtd'].str.replace(' ', '')
+            data['Qtd'] = pd.to_numeric(data['Qtd'], errors='coerce')
+            
             #Concateno em um dataframe, pois assim permito que o usuário faça a importação de diversos CSVs, um para cada ano
             df = pd.concat([df, data])
 
@@ -88,11 +94,12 @@ def filtra_df(df):
         if st.session_state.filter_cont != 'Todos':
             df = df.loc[df['Continente'] == st.session_state.filter_cont]
 
+        df_filt = df.copy()
         #Crio o filtro de colunas
         st.session_state.list_colunas = st.sidebar.multiselect('Selecione as colunas', df.columns, st.session_state.list_colunas)
 
         #Aplico o filtro
-        df_filt = df[st.session_state.list_colunas]
+        df_filt_tab = df[st.session_state.list_colunas]
 
         #Adicionando a barra de progresso
         my_bar = st.progress(0, text="Carregando a Visualização...")
@@ -102,22 +109,22 @@ def filtra_df(df):
         time.sleep(0.5)
         my_bar.empty()
     
-        return df_filt
+        return df_filt, df_filt_tab
 
-def exibe_tabelas(df_filt):
+def exibe_tabelas(df):
     st.write('## Tabela Comum')
     st.dataframe(df)
 
     st.write('## Tabela Interativa')
-    if df_filt is not None:
-        gb = GridOptionsBuilder.from_dataframe(df_filt)
+    if df is not None:
+        gb = GridOptionsBuilder.from_dataframe(df)
         gb.configure_default_column(editable=False, groupable=True)
         gb.configure_selection('multiple', use_checkbox=True)
         gridOptions = gb.build()
 
-        AgGrid(df_filt, gridOptions=gridOptions, enable_enterprise_modules=True)
+        AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True)
 
-def graf_simples(df):
+def graficos(df):
     # Criando o gráfico de pizza
     fig = px.pie(
         df,
@@ -132,15 +139,47 @@ def graf_simples(df):
 
     st.plotly_chart(fig)
 
-    #Criando Gráfico de linhas
-    cmap = {'Janeiro':1, 'Fevereiro':2,'Março':3,'Abril':4,'Maio':5, 'Junho':6,'Julho': 7,'Agosto': 8,'Setembro': 9,'Outubro': 10, 'Novembro': 11, 'Dezembro': 12}
-    df['Mes'] = df['Mes'].map(cmap)
-    df['Data'] = pd.to_datetime(df['Ano'].astype(str) + '-' + df['Mes'].astype(str) + '-01', format='%Y-%m-%d')
-    df_ag= df.groupby('Data', as_index=False)['Qtd'].sum()
+    #Criando Gráfico de Barras
+    fig = px.bar(
+        df,
+        x='Pais',
+        y='Qtd',
+        title='Quantidade de Turistas por Pais',
+        labels={'Qtd Tutistas': 'Qtd'}
+    )
 
-    fig = px.line(df_ag, x='Data', y='Qtd', title='Qtd Turistas por Mês')
     st.plotly_chart(fig)
 
+    #Criando histrograma
+    fig = px.histogram(
+        df,
+        x='Qtd',
+        title='Distribuição da Qtd de Turistas',
+        labels={'Qtd': 'Quantidade de Turistas'},
+        nbins=20
+    )
+
+    st.plotly_chart(fig)
+
+def metricas(df):
+    #Qtd linhas
+    qtd_linhas = df.shape[0]
+    
+    #Média de passageiros
+    media_psg = round(df['Qtd'].mean(),0)
+
+    #Total de passageiros
+    total_psg = df['Qtd'].sum()
+
+    # Exibindo as métricas
+    a,b,c = st.columns(3)
+
+    with a:
+        st.metric(label="Qtd Registros", value=qtd_linhas)
+    with b:
+        st.metric(label="Média Passageiros por Registro", value=media_psg)
+    with c:
+        st.metric(label="Total de Passageiros", value=total_psg)
 
 ################### INICIANDO O APP ######################
 
@@ -151,9 +190,11 @@ start = time.time()
 
 if files:
     df, csv = tratamento(files)
-    df_filt = filtra_df(df)
-    exibe_tabelas(df_filt)
+    df_filt, df_filt_tab = filtra_df(df)
+    metricas(df_filt)
+    graficos(df_filt)
+    exibe_tabelas(df_filt_tab)
     st.download_button(label="Baixar Dados", data=csv, file_name="base.csv", mime="text/csv")
-    graf_simples(df)
 end = time.time()
+
 st.write(f'Tempo de execução: {end - start:.2f} segundos')
